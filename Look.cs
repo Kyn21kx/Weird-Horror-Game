db98508c168321ba0c0994a1911b87e3ce04d6b9
Kyn21kx <nefesmeralda@gmail.com>
2018-08-10 21:52:30 -0500

    Mouse Look Script

---
+++ Look.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Look : MonoBehaviour {
+    #region Variables
+    public Transform Player;
+    float Sensitivity = 3f;
+    float xAxisClamp = 0;
+    #endregion
+    //Locks the cursor at the beggining of the game
+    private void Start() {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    //Controls Methods and updates once per physics interaction
+    private void FixedUpdate() {
+        RotateCamera();
+        
+    }
+    //Manages Camera's Rotation
+    void RotateCamera() {
+        float MouseX = Input.GetAxis("Mouse X");
+        float MouseY = Input.GetAxis("Mouse Y");
+        float RotAmntX = MouseX * Sensitivity;
+        float RotAmntY = MouseY * Sensitivity;
+        Vector3 TargetRot = transform.rotation.eulerAngles;
+        Vector3 TargetRotPl = Player.rotation.eulerAngles;
+        TargetRot.x -= RotAmntY;
+        TargetRotPl.y += RotAmntX;
+        TargetRot.z = 0;
+        xAxisClamp -= RotAmntY;
+        
+        if (xAxisClamp > 90) {
+            xAxisClamp = 90;
+            TargetRot.x = 90;
+        }
+        else if (xAxisClamp < -90) {
+            xAxisClamp = -90;
+            TargetRot.x = 270;
+        }
+
+        transform.rotation = Quaternion.Euler(TargetRot);
+        Player.rotation = Quaternion.Euler(TargetRotPl);
+    }
+}
