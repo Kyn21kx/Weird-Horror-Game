5ec39e6773892f3aeb66a020fc131b17d93eb324
Kyn21kx <nefesmeralda@gmail.com>
2018-08-10 21:54:07 -0500

    Basic Movement (No function to control Running)

---
+++ Move.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+    #region Variables
+    CharacterController characterController;
+    float walkSpeed = 5f;
+    float runSpeed;
+    float jumpSpeed;
+    #endregion
+
+    private void Start() {
+        characterController = this.gameObject.GetComponent<CharacterController>();
+    }
+
+    private void FixedUpdate() {
+        Movement();
+    }
+
+    void Movement() {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        Vector3 movDirX = transform.right * h * walkSpeed;
+        Vector3 movDirF = transform.forward * v * walkSpeed;
+
+        characterController.SimpleMove(movDirX);
+        characterController.SimpleMove(movDirF);
+    }
+
+}
